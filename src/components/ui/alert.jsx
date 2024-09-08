eca5a03cc5c239e5d68371c9096138f0274ca7f0
ShedrachIkenna <Shedrach686@gmail.com>
2024-09-08 16:10:38 +0100

    Added button component an fixed alert component error

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -1,24 +1,24 @@
 import * as React from "react"
 import { cva } from "class-variance-authority";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils"  // Add this import
 
 const alertVariants = cva(
-    "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
-    {
-      variants: {
-        variant: {
-          default: "bg-background text-foreground",
-          destructive:
-            "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
-        },
+  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  {
+    variants: {
+      variant: {
+        default: "bg-background text-foreground",
+        destructive:
+          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
       },
-      defaultVariants: {
-        variant: "default",
-      },
-    }
-  )
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
 
-  const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
+const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
     <div
       ref={ref}
       role="alert"
@@ -27,3 +27,23 @@ const alertVariants = cva(
     />
   ))
   Alert.displayName = "Alert"
+
+const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
+
+const AlertDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
