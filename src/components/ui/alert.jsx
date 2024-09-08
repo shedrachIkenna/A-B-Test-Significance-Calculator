18554bef4e5ae4eb675cb4f07cac9be10dbb8d67
ShedrachIkenna <Shedrach686@gmail.com>
2024-09-08 16:26:03 +0100

    Added the input component

--- src/components/ui/alert.jsx
+++ src/components/ui/alert.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cva } from "class-variance-authority";
-import { cn } from "@/lib/utils"  // Add this import
+import { cn } from "@/lib/utils"
 
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
@@ -19,14 +19,14 @@ const alertVariants = cva(
 )
 
 const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
-    <div
-      ref={ref}
-      role="alert"
-      className={cn(alertVariants({ variant }), className)}
-      {...props}
-    />
-  ))
-  Alert.displayName = "Alert"
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
+))
+Alert.displayName = "Alert"
 
 const AlertTitle = React.forwardRef(({ className, ...props }, ref) => (
   <h5
